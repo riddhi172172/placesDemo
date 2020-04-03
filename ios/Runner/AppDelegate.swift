888cbafe30bdda678dd39fc8de99717424857e2d
Riddhi Shankar <riddhi@iroidsolutions.com>
2020-04-03 14:25:43 +0530

    ios setup done for auth and database

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,16 +3,13 @@ import Flutter
 import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: 
-    
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    FirebaseApp.configure()
+//    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    
 }
