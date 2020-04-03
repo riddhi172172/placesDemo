98bda8fbcc3e43aa1ce50694f5551d372198bbe3
Riddhi Shankar <riddhi@iroidsolutions.com>
2020-04-03 14:36:44 +0530

    unnecessary code removed

--- lib/helper/prefkeys.dart
+++ lib/helper/prefkeys.dart
@@ -1,8 +1,5 @@
 class PrefKeys {
   static var user = "user";
   static var userId = "userId";
-  static var language = "language";
   static var isUserLogin = "isUserLogin";
-  static var latitude = "latitude";
-  static var longitude = "longitude";
 }
